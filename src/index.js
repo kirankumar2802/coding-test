abf5132859b783bdb7d093786222ded6722be8b1
Your Name <you@example.com>
2021-01-25 13:32:42 +0530

    coding

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import './components/Main.css';
+import './components/errormodal.css';
+import './components/callout.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
